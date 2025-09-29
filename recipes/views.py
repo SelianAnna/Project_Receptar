9d766152b31e98341791fbb51baa2f2a00a6bb38
SelianAnna <anna.selianova@gmail.com>
2025-09-30 00:47:51 +0200

    working version pridanie ingrediencii

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Q
-from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Recipe, Ingredient
+from django.db.models import Q
+from .models import Recipe, Ingredient, RecipeIngredient
 from .forms import RecipeForm
 
 class RecipeListView(ListView):
@@ -24,8 +23,34 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy("recipes:list")
 
     def form_valid(self, form):
-        self.object = form.save(user=self.request.user)
+        self.object = form.save(commit=False)
+        self.object.created_by = self.request.user
+        self.object.save()
+
+        # Získa zoznam ingrediencií z POST dát (arrays)
+        names = self.request.POST.getlist('ingredient_name')
+        amounts = self.request.POST.getlist('ingredient_amount')
+        for name, amount in zip(names, amounts):
+            if name.strip():
+                ing, _ = Ingredient.objects.get_or_create(name=name.strip())
+                RecipeIngredient.objects.create(recipe=self.object, ingredient=ing, amount=amount.strip())
+
         return super().form_valid(form)
+
+class RecipeUpdateView(LoginRequiredMixin, UpdateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = "recipes/recipe_form.html"
+    success_url = reverse_lazy("recipes:list")
+    def get_queryset(self):
+        return Recipe.objects.filter(created_by=self.request.user)
+
+class RecipeDeleteView(LoginRequiredMixin, DeleteView):
+    model = Recipe
+    template_name = "recipes/recipe_confirm_delete.html"
+    success_url = reverse_lazy("recipes:list")
+    def get_queryset(self):
+        return Recipe.objects.filter(created_by=self.request.user)
 
 class IngredientListView(ListView):
     model = Ingredient
@@ -37,35 +62,14 @@ class RecipeSearchView(ListView):
     model = Recipe
     template_name = "recipes/recipe_search.html"
     context_object_name = "recipes"
-
     def get_queryset(self):
         qs = Recipe.objects.all()
         q = self.request.GET.get("q", "").strip()
         ingredients = self.request.GET.get("ingredients", "").strip()
-
         if q:
             qs = qs.filter(Q(title__icontains=q) | Q(description__icontains=q) | Q(instructions__icontains=q))
-
         if ingredients:
             tokens = [t.strip() for t in ingredients.split(",") if t.strip()]
             for t in tokens:
-                qs = qs.filter(ingredients__name__iexact=t)
-
+                qs = qs.filter(ingredients__name__icontains=t)
         return qs.distinct()
-
-class RecipeUpdateView(LoginRequiredMixin, UpdateView):
-    model = Recipe
-    form_class = RecipeForm
-    template_name = "recipes/recipe_form.html"
-    success_url = reverse_lazy("recipes:list")
-
-    def get_queryset(self):
-        return Recipe.objects.filter(created_by=self.request.user)
-
-class RecipeDeleteView(LoginRequiredMixin, DeleteView):
-    model = Recipe
-    template_name = "recipes/recipe_confirm_delete.html"
-    success_url = reverse_lazy("recipes:list")
-
-    def get_queryset(self):
-        return Recipe.objects.filter(created_by=self.request.user)
